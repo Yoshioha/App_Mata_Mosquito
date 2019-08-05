c86945e60fd84f3ad828b3bd89820ede19ffa24e
Daniel Yoshio <danielyoshio2013@gmail.com>
2019-08-05 09:02:21 -0300

    Controlando os pontos de vida

--- codigos/jogo.js
+++ codigos/jogo.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
@@ -14,6 +15,14 @@ function posicaoRandomica(){
     //remover mosquito anterior(caso exista)
     if(document.getElementById('mosquito')){
         document.getElementById('mosquito').remove()
+
+        if(vidas > 3){
+
+        }else{
+            document.getElementById('v' + vidas).src='../imagens/coracao_vazio.png'
+            vidas++
+        }
+        
     }
     
 
@@ -34,6 +43,9 @@ function posicaoRandomica(){
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
 
